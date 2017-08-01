09c52c043294e3a2229291c590fa554850a08832
chiragmakkar <chirag.piet@gmail.com>
2017-08-02 00:28:19 +0530

    chirag

--- models/mongodb.js
+++ models/mongodb.js
@@ -1,86 +1,15 @@
 var mongoose = require('mongoose')
 var schema = mongoose.Schema;
-
-//Entrepenuer Profile,prefix e represents entrepenuer
-var entreProfile = new Schema({
-    "eFirstName": String,
-    "eLastName": String,
-    "eEmail": String,
-    "ePhone": Number,
-    "ePassword": String,
-    "eEmail_verified": Boolean,
-    "ePhone_verified": Boolean,
-    "eOTP_Mobile": Number,
-    "eEmailCode": String,
-    "eOrganisation": String,
-    "eWebsite": String,
-    "eAddress": String,
-    "eAboutUs": String,
-    "evision": String,
-    "eFoundedOn": String,
-    "eIndustryType": String,
-});
-//intern profile,prefix i represents intern
-var internProfile = new Schema({
-    "iFirstName": String,
-    "iLastName": String,
-    "iEmail": String,
-    "iPhone": Number,
-    "iPassword": String,
-    "iEmail_verified": Boolean,
-    "iPhone_verified": Boolean,
-    "iOTP_Mobile": Number,
-    "iEmailCode": String,
-    "iIntroduction": String,
-    "iFatherName": String,
-    "iWebsite": String,
-    "iLinkedIn": String,
-    "iFacebook": String,
-    "iSkills":String,
-    "iExperience": [Schema.Types.Mixed],
-    "iEducation": {
-        "university": String,
-        "course": String,
-        "fromMonth": String,
-        "fromYear": String,
-        "toMonth": String,
-        "toYear": String
-    }
+var mongoose = require('mongoose');
+var db = mongoose.connect('mongodb://admin:admin@ds129053.mlab.com:29053/neie');
+mongoose.connection.once('connected', function() {
+	console.log("Connected to database")
 });
 
-//post a project
-var postAProject = new Schema({
-    "typeOfWork": String,
-    "describeYourJob": String,
-    "projectName": String,
-    "skillsrequired": [Schema.Types.Mixed],
-    "describeYourProject": String,
-    "cost": String,
-    "duration": String
-});
+var entreProfile = require('./entreprofile.js');
+var internProfile = require('./internprofile');
+var expertAdvice = require('./liveexpert.js');
+var postAProject = require('./postaproject.js');
+var query = require('./query.js');
+var reviewIntern = require('./reviewintern.js');
 
-//Live Expert Advice Schema for Chat
-var liveExpert = new Schema({
-    "chooseDomain": String,
-    "yourQuestion": String,
-    "describeChatQuery": String,
-    "describeExpectation": String
-
-});
-//Raise a Query Schema 
-var query = new Schema({
-    "queryDomain": String,
-    "describeDiscussionQuery": String,
-    "discussionExpectation": String
-});
-//Review Intern Schema
-var reviewIntern = new Schema({
-    "rating": Number,
-    "review": String
-});
-module.exports = mongoose.model('entreProfile', entreProfile);
-module.exports = mongoose.model('internProfile', internProfile);
-module.exports = mongoose.model('postAProject', postAProject);
-module.exports = mongoose.model('expertAdvice', expertAdvice);
-module.exports = mongoose.model('query', query);
-module.exports = mongoose.model('reviewIntern', reviewIntern);
--- server.js
+++ server.js
@@ -2,14 +2,13 @@ const express = require('express');
 const app = express();
 const bodyParser = require("body-parser");
 
+
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
   extended:true
 }));
 
-app.get('/', function(req, res){
-	res.send("NEIE version 2 is now live");
-});
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
