e0edc1a37a951dfbdca0d5c45329927b4c7f5f2d
chiragmakkar <chirag.piet@gmail.com>
2017-08-03 13:32:32 +0530

    minor omit

--- get.js
+++ get.js
@@ -1,9 +1,9 @@
-module.exports = function(router, mon) {
+module.exports = function(router, mongo) {
 
 router.get('/', function(req, res) {
   console.log('You are on the homepage');
   console.log(__dirname);
-  res.sendFile( __dirname+'/views/dashboardEntreprneur.html');
+  res.sendFile( __dirname+'/views/dashboardEntrepreneur.html');
 });
 
 }
