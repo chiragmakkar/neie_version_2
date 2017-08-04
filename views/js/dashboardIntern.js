1b4da4ae9a44cbdfbb813959091fdfde8908724f
anuj25jain25 <anuj25jain25@gmail.com>
2017-08-05 02:09:06 +0530

    adding intern page

---
+++ views/js/dashboardIntern.js
@@ -0,0 +1,70 @@
+var app = angular.module("Neiex", ['ngRoute']);
+app.config(function ($routeProvider) {
+
+	//Search Page
+	$routeProvider.when('/search', {
+		templateUrl: "views/search.html",
+		controller: 'SearchCtrl'
+	});
+	//Blank Page
+	$routeProvider.when('/blank', {
+		templateUrl: "views/blank.html"
+	});
+	//Post Project
+	$routeProvider.when('/postProject', {
+		templateUrl: "views/postProject.html"
+	});
+	//Live Expert
+	$routeProvider.when('/liveExpert', {
+		templateUrl: "views/liveExpert.html"
+	});
+	//Raise a query
+	$routeProvider.when('/raiseAQuery', {
+		templateUrl: "views/raiseAQuery.html"
+	});
+	//My Experience
+	$routeProvider.when('/myExperience', {
+		templateUrl: "views/myExperience.html"
+	});
+	//Profile Page
+	$routeProvider.when('/profile', {
+		templateUrl: "views/profileIntern.html",
+		controller: 'ProfileCtrl'
+	});
+	//Signup Page
+	$routeProvider.when('/signup', {
+		templateUrl: "views/signUp.html",
+		controller: 'SignupCtrl'
+	});
+	//Default page
+	$routeProvider.otherwise({
+		redirectTo: '/blank'
+	});
+});
+//Controller for Search Page
+app.controller('SearchCtrl', function ($location, $scope) {
+	$scope.bodyClick = function () {
+		$location.path('/blank');
+	}
+});
+//Controller for Profile Page
+app.controller('ProfileCtrl', function ($location, $scope) {
+	//Body click
+	$scope.bodyClick = function () {
+		$location.path('/blank');
+	};
+	//User Settings Function
+	$scope.userSettings = function () {
+		$scope.userProfileOptions = 'settings';
+		//Default Profile Setting Display
+		$scope.userSettingsOptions = 'profile';
+	};
+	//Profile Settings Show Function
+	$scope.profileSettings = function () {
+		$scope.userSettingsOptions = 'profile';
+	};
+	//Account Settings Show Function
+	$scope.accountSettings = function () {
+		$scope.userSettingsOptions = 'account';
+	};
+});
